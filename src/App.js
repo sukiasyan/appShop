e0ab179e32b9517f9ff6b9a9d2222d39e62ffa25
hsukiasyan <h.sukiasyan@gethatch.com>
2022-11-14 19:02:49 +0100

    add allinone page

--- src/App.js
+++ src/App.js
@@ -2,6 +2,7 @@ import Hero from './components/Hero';
 import Navbar from './components/Navbar';
 import About from './components/About';
 import Support from './components/Support';
+import AllInOne from './components/AllInOne';
 
 function App() {
   return (
@@ -10,6 +11,7 @@ function App() {
       <Hero />
       <About />
       <Support />
+      <AllInOne />
     </>
   );
 }
---
+++ src/components/AllInOne.jsx
@@ -0,0 +1,143 @@
+import { CheckIcon } from '@heroicons/react/24/outline';
+import React from 'react';
+
+const AllInOne = () => {
+  return (
+    <div className='w-full my-32'>
+      <div className='max-w-[1240px] mx-auto px-2'>
+        <h2 className='text-5xl font-bold text-center'>All-In-One Platform</h2>
+        <p className='text-2xl py-8 text-grey-500 text-center'>
+          Lorem ipsum dolor sit amet consectetur adipisicing elit. Amet, sunt
+          harum nulla voluptatibus nihil porro rerum repellat possimus quos
+          veritatis iure exercitationem ex doloribus accusamus nesciunt aliquid,
+          distinctio velit. Aliquam?
+        </p>
+        <div className='grid sm:grid-cols-2 lg:grid-cols-4 gap-4 pt-4'>
+          <div className='flex'>
+            <div>
+              <CheckIcon className='w-7 mr-4 text-green-600' />
+            </div>
+
+            <div>
+              <h3 className='font-bold text-lg'>Notifications</h3>
+              <p className='text-lg pt-2 pb-4'>
+                Lorem ipsum dolor sit amet, consectetur adipisicing elit.
+                Asperiores vero amet fuga quisquam quasi deserunt porro,
+                provident laborum sit neque ratione et reprehenderit eius fugit
+                praesentium deleniti autem nihil aspernatur?
+              </p>
+            </div>
+          </div>
+
+          <div className='flex'>
+            <div>
+              <CheckIcon className='w-7 mr-4 text-green-600' />
+            </div>
+
+            <div>
+              <h3 className='font-bold text-lg'>Notifications</h3>
+              <p className='text-lg pt-2 pb-4'>
+                Lorem ipsum dolor sit amet, consectetur adipisicing elit.
+                Asperiores vero amet fuga quisquam quasi deserunt porro,
+                provident laborum sit neque ratione et reprehenderit eius fugit
+                praesentium deleniti autem nihil aspernatur?
+              </p>
+            </div>
+          </div>
+          <div className='flex'>
+            <div>
+              <CheckIcon className='w-7 mr-4 text-green-600' />
+            </div>
+
+            <div>
+              <h3 className='font-bold text-lg'>Notifications</h3>
+              <p className='text-lg pt-2 pb-4'>
+                Lorem ipsum dolor sit amet, consectetur adipisicing elit.
+                Asperiores vero amet fuga quisquam quasi deserunt porro,
+                provident laborum sit neque ratione et reprehenderit eius fugit
+                praesentium deleniti autem nihil aspernatur?
+              </p>
+            </div>
+          </div>
+          <div className='flex'>
+            <div>
+              <CheckIcon className='w-7 mr-4 text-green-600' />
+            </div>
+
+            <div>
+              <h3 className='font-bold text-lg'>Notifications</h3>
+              <p className='text-lg pt-2 pb-4'>
+                Lorem ipsum dolor sit amet, consectetur adipisicing elit.
+                Asperiores vero amet fuga quisquam quasi deserunt porro,
+                provident laborum sit neque ratione et reprehenderit eius fugit
+                praesentium deleniti autem nihil aspernatur?
+              </p>
+            </div>
+          </div>
+          <div className='flex'>
+            <div>
+              <CheckIcon className='w-7 mr-4 text-green-600' />
+            </div>
+
+            <div>
+              <h3 className='font-bold text-lg'>Notifications</h3>
+              <p className='text-lg pt-2 pb-4'>
+                Lorem ipsum dolor sit amet, consectetur adipisicing elit.
+                Asperiores vero amet fuga quisquam quasi deserunt porro,
+                provident laborum sit neque ratione et reprehenderit eius fugit
+                praesentium deleniti autem nihil aspernatur?
+              </p>
+            </div>
+          </div>
+          <div className='flex'>
+            <div>
+              <CheckIcon className='w-7 mr-4 text-green-600' />
+            </div>
+
+            <div>
+              <h3 className='font-bold text-lg'>Notifications</h3>
+              <p className='text-lg pt-2 pb-4'>
+                Lorem ipsum dolor sit amet, consectetur adipisicing elit.
+                Asperiores vero amet fuga quisquam quasi deserunt porro,
+                provident laborum sit neque ratione et reprehenderit eius fugit
+                praesentium deleniti autem nihil aspernatur?
+              </p>
+            </div>
+          </div>
+          <div className='flex'>
+            <div>
+              <CheckIcon className='w-7 mr-4 text-green-600' />
+            </div>
+
+            <div>
+              <h3 className='font-bold text-lg'>Notifications</h3>
+              <p className='text-lg pt-2 pb-4'>
+                Lorem ipsum dolor sit amet, consectetur adipisicing elit.
+                Asperiores vero amet fuga quisquam quasi deserunt porro,
+                provident laborum sit neque ratione et reprehenderit eius fugit
+                praesentium deleniti autem nihil aspernatur?
+              </p>
+            </div>
+          </div>
+          <div className='flex'>
+            <div>
+              <CheckIcon className='w-7 mr-4 text-green-600' />
+            </div>
+
+            <div>
+              <h3 className='font-bold text-lg'>Notifications</h3>
+              <p className='text-lg pt-2 pb-4'>
+                Lorem ipsum dolor sit amet, consectetur adipisicing elit.
+                Asperiores vero amet fuga quisquam quasi deserunt porro,
+                provident laborum sit neque ratione et reprehenderit eius fugit
+                praesentium deleniti autem nihil aspernatur?
+              </p>
+            </div>
+          </div>
+        </div>
+      </div>
+    </div>
+  );
+};
+
+export default AllInOne;
